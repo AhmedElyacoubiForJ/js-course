c71adbc53cb0320b9d3dc50f4f6729fe965044c4
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-21 15:24:47 +0100

    comments & variables

---
+++ hello-world.js
@@ -0,0 +1,17 @@
+console.log('Hello World Learning JS :)')
+console.log("Hola modo")
+console.log(10 + 10);
+
+//  Single comment
+
+/* 
+  Multi
+  Line
+  Comments 
+*/
+
+/* 
+    how to configure rules like  "semi" and "quotes"
+
+    https://eslint.org/docs/latest/use/getting-started#configuration
+*/
---
+++ variables.js
@@ -0,0 +1,17 @@
+// Variables
+var firstName = "Jamila";
+var age = 21;
+var isFeamle = true;
+var balance = 100.32;
+// 30 of january
+var dob = new Date(2000, 0, 30);
+var person = {};
+var empty = undefined;
+
+console.log(firstName);
+console.log(age);
+console.log(isFeamle);
+console.log(balance)
+console.log(dob)
+console.log(person);
+console.log(empty)
