ff4f8f6a4876ad63ecbdc4e343202803807c701b
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-22 20:14:34 +0100

    build in functions & loops

---
+++ basics/built-in-functions.js
@@ -0,0 +1,12 @@
+// Built in Functions
+var person = {
+    name: "Jamila"
+}
+console.log(Object.values(person));
+console.log(Object.keys(person));
+
+console.log("Jamila".toUpperCase());
+console.log("Jamila".toLocaleLowerCase());
+console.log("Jamila".indexOf("i"));
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
--- basics/functions.js
+++ basics/functions.js
@@ -9,13 +9,3 @@ var result2 = addNumbers(7, 3);
 
 console.log(result1);
 console.log(result2);
-
-var person = {
-    name: "Jamila"
-}
-console.log(Object.values(person));
-console.log(Object.keys(person));
-
-console.log("Jamila".toUpperCase());
-console.log("Jamila".toLocaleLowerCase());
-console.log("Jamila".indexOf("i"));
