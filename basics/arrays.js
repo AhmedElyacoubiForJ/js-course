16141bf159528cb0bc5e6ab37e8367dfb40680f6
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-22 15:34:11 +0100

    boolean & arrays

---
+++ basics/arrays.js
@@ -0,0 +1,14 @@
+// Arrays
+var names = [
+    "Marcus",
+    "Jamila", 
+    "Mike", 
+    "Aisha"
+];
+console.log(names);
+console.log("Size = " + names.length);
+console.log("Index 0 - " + names[0]);
+console.log("Index 1 - " + names[1]);
+console.log("Index 2 - " + names[2]);
+console.log("Index 3 - " + names[3]);
+console.log("Index 4 - " + names[4]);
