c98be009f7b3d20ada6122aadca24d6e9d9e53fd
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-24 18:03:01 +0100

    hoistind & let keyword

---
+++ basics/hoisting.js
@@ -0,0 +1,6 @@
+// Hoisting
+// is like var i;
+for (var i = 0; i <= 10; i++) {
+   console.log(i);
+}
+console.log("value of i outside a loop: " + i);
---
+++ basics/let-keyword.js
@@ -0,0 +1,7 @@
+// Let
+// Hosting don't taking place 
+for (let i = 0; i <= 10; i++) {
+   console.log(i);
+}
+// Error
+//console.log("value of i outside a loop: " + i);
