bc6f54792c81a896cc7f73961f694d86e412f545
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-24 17:49:43 +0100

    switch statement

---
+++ basics/switch-statement.js
@@ -0,0 +1,22 @@
+// Switch
+var gender = 'M';
+
+switch (gender) {
+    case 'M':
+        console.log('Male');
+        break;
+    case "F":
+        console.log('Female');
+        break;
+    default:
+        console.log('Unkown');
+        break;
+}
+
+// if (gender == 'M') {
+//     console.log('Male');
+// } else if (gender == 'F' || gender == 'FEMALE') {
+//     console.log('Female');
+// } else {
+//     console.log('Unkown');
+// }
