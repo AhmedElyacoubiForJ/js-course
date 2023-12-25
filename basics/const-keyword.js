0b927557a813c378593fa0de2feedc4f784165a2
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-12-25 14:35:27 +0100

    const keyword

---
+++ basics/const-keyword.js
@@ -0,0 +1,24 @@
+// const
+
+let brand1 = "Amigoscode";
+console.log(brand1);
+brand1 = {};
+console.log(brand1);
+brand1 = 10;
+console.log(brand1);
+brand1 = function() {
+    return "Hello";
+}
+console.log(brand1)
+console.log(brand1());
+
+const brand2 = "Amigoscode";
+// not allowed
+// brand = 10;
+console.log(brand2);
+
+const brandObject = {};
+brandObject["brand"] = brand2;
+console.log(brandObject);
+delete brandObject.brand;
+console.log(brandObject);
